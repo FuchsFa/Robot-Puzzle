ed748435f80ef31d83e3da040526642c3b3a5140
FuchsFa <fabian.tj75@gmail.com>
2018-03-22 13:37:16 +0100

    BasicLeg und BasicSensor erstellt
    Grundlegende Funktionalität für beide Klassen wurde hinzugefügt.

--- Robot-Puzzle/Assets/Scripts/Model/RobotParts/BasicLeg.cs
+++ Robot-Puzzle/Assets/Scripts/Model/RobotParts/BasicLeg.cs
@@ -1,16 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicLeg : MonoBehaviour {
+public class BasicLeg : RobotPart {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //TODO: TerrainType hinzufügen
+
+    /// <summary>
+    /// Erstellt das Bein und fügt es dem angegebenen Roboer hinzu.
+    /// </summary>
+    /// <param name="robot"></param>
+    public BasicLeg(Robot robot) {
+        type = PartType.Mobility;
+        robot.AddPart(this);
+    }
+
+    /// <summary>
+    /// Default Constructor.
+    /// </summary>
+    public BasicLeg() {
+        type = PartType.Mobility;
+    }
+
+    /// <summary>
+    /// Speichert den Roboter, zu dem dieses Teil hinzugefügt wird in der 'attachedTo'-Variable.
+    /// </summary>
+    /// <param name="robot"></param>
+    public override void AddTo(Robot robot) {
+        attachedTo = robot;
+    }
+
+    /// <summary>
+    /// Gibt ein Dictionary zurück mit allen Funktionen, die ein Roboter durch dieses Teil ausführen kann.
+    /// </summary>
+    /// <returns></returns>
+    public override Dictionary<string, Action> GetActionList() {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Setzt die 'attachedTo'-Variable auf null zurück
+    /// </summary>
+    /// <param name="robot"></param>
+    public override void RemoveFrom(Robot robot) {
+        attachedTo = null;
+    }
+
+    /// <summary>
+    /// Lässt den Roboter einen Schritt in Blickrichtung gehen.
+    /// </summary>
+    public void Walk() {
+        //TODO: implementieren
+    }
 }
--- Robot-Puzzle/Assets/Scripts/Model/RobotParts/BasicSensor.cs
+++ Robot-Puzzle/Assets/Scripts/Model/RobotParts/BasicSensor.cs
@@ -1,16 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicSensor : MonoBehaviour {
+public class BasicSensor : RobotPart {
+    private InteractiveObject sensedObject;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// Erstellt den Sensor und fügt ihn dem angegebenen Roboter hinzu.
+    /// </summary>
+    /// <param name="robot"></param>
+    public BasicSensor(Robot robot) {
+        type = PartType.Sensor;
+        robot.AddPart(this);
+    }
+
+    /// <summary>
+    /// Default Constructor.
+    /// </summary>
+    public BasicSensor() {
+        type = PartType.Sensor;
+    }
+
+    /// <summary>
+    /// Speichert den Roboter, zu dem dieses Teil hinzugefügt wird in der 'attachedTo'-Variable.
+    /// </summary>
+    /// <param name="robot"></param>
+    public override void AddTo(Robot robot) {
+        attachedTo = robot;
+    }
+
+    /// <summary>
+    /// Gibt ein Dictionary zurück mit allen Funktionen, die ein Roboter durch dieses Teil ausführen kann.
+    /// </summary>
+    /// <returns></returns>
+    public override Dictionary<string, Action> GetActionList() {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Setzt die 'attachedTo'-Variable auf null zurück
+    /// </summary>
+    /// <param name="robot"></param>
+    public override void RemoveFrom(Robot robot) {
+        attachedTo = null;
+    }
+
+    /// <summary>
+    /// Überprüft, ob auf dem Feld vor dem Roboter ein interaktives Objekt liegt.
+    /// </summary>
+    /// <returns>Das Objekt. Gibt null zurück, falls kein Objekt gefunden wurde.</returns>
+    public InteractiveObject Sense() {
+        sensedObject = null;
+        //TODO: implementieren
+        return sensedObject;
+    }
 }
