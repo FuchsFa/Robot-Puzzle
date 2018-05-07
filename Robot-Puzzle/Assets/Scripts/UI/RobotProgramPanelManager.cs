69437a43831d8e3909a0dd99d3b5a1e7dc13c20e
FuchsFa <fabian.tj75@gmail.com>
2018-05-07 14:27:53 +0200

    Zeilenzahlen Anzeige
    Die Zeilenzahlen für den code Editor werden nun korrekt angezeigt. Außerdem wurden die ersten Schritte für Syntax highlighting erledigt.

--- Robot-Puzzle/Assets/Scripts/UI/RobotProgramPanelManager.cs
+++ Robot-Puzzle/Assets/Scripts/UI/RobotProgramPanelManager.cs
@@ -10,10 +10,22 @@ public class RobotProgramPanelManager : MonoBehaviour {
     private TMP_InputField editor;
 
     [SerializeField]
+    private TextMeshProUGUI lineNumberText;
+
+    [SerializeField]
+    private ScrollRect lineNumberScrollRect;
+
+    [SerializeField]
     private Button saveButton;
 
     [SerializeField]
     private Button discardButton;
+
+    [SerializeField]
+    private Color highlightedColor;
+
+    [SerializeField]
+    private List<string> wordsToHighlight;
 
     //Der Script code des ausgewählten Roboters ohne Änderungen.
     private string oldScriptCode;
@@ -24,6 +36,44 @@ public class RobotProgramPanelManager : MonoBehaviour {
     public void OnSelectRobot() {
         oldScriptCode = RobotManager.Instance.selectedRobot.GetComponent<Robot>().GetScriptCode();
         editor.text = oldScriptCode;
+    }
+
+    /// <summary>
+    /// Passt die Zeilenzahlen an den Code im Editor an.
+    /// </summary>
+    public void AdjustLineNumbers() {
+        string temp = "";
+        string[] codeLines = editor.text.Split('\n');
+        for (int i = 0; i < codeLines.Length; i++) {
+            temp += (i + 1) + "\n";
+        }
+
+        lineNumberText.text = temp;
+    }
+
+    public void CheckSyntaxForWholeText() {
+        TMP_TextInfo textInfo = editor.textComponent.textInfo;
+        TMP_WordInfo wordInfo = textInfo.wordInfo[1];
+        TMP_CharacterInfo charInfo;
+
+        for (int i = 0; i < wordInfo.characterCount; i++) {
+            charInfo = textInfo.characterInfo[wordInfo.firstCharacterIndex + i];
+            charInfo.color = highlightedColor;
+        }
+        editor.textComponent.ForceMeshUpdate();
+    }
+
+    /// <summary>
+    /// Überprüft, ob das übergebene Wort hervorgehoben werden soll.
+    /// Wenn ja, dann gibt es das Wort zusammen mit den nötigen Rich-Text-Tags zurück, sonst wird nur das Wort zurückgegeben
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    private string CheckSyntax(string text) {
+        if(wordsToHighlight.Contains(text)) {
+            text = "<color= #" + ColorUtility.ToHtmlStringRGB(highlightedColor) + ">" + text + "</color>";
+        }
+        return text;
     }
 
     /// <summary>
@@ -39,5 +89,16 @@ public class RobotProgramPanelManager : MonoBehaviour {
     /// </summary>
     public void DiscardChanges() {
         editor.textComponent.text = oldScriptCode;
+    }
+
+    private void Update() {
+        AdjustLineNumberScrollPosition();
+    }
+
+    /// <summary>
+    /// Passt die derzeitige Scroll-Position des LineNumber Felds an das Editor-Textfeld an.
+    /// </summary>
+    private void AdjustLineNumberScrollPosition() {
+        lineNumberScrollRect.verticalNormalizedPosition = 1 - editor.verticalScrollbar.value;
     }
 }
