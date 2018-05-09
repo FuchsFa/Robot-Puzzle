16d8e5d20c9b1bd3fb5fdf6174e8d4672da768b1
FuchsFa <fabian.tj75@gmail.com>
2018-05-09 14:47:54 +0200

    Syntax Highlighting
    Der ingame Skript-Editor hebt nun Lua-Keywords und Zahlen korrekt hervor

--- Robot-Puzzle/Assets/Scripts/UI/RobotProgramPanelManager.cs
+++ Robot-Puzzle/Assets/Scripts/UI/RobotProgramPanelManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,10 +23,16 @@ public class RobotProgramPanelManager : MonoBehaviour {
     private Button discardButton;
 
     [SerializeField]
-    private Color32 highlightedColor;
+    private Color32 keywordColor;
+
+    private List<string> keywords;
 
     [SerializeField]
-    private List<string> wordsToHighlight;
+    private Color32 commentColor;
+    [SerializeField]
+    private Color32 stringColor;
+    [SerializeField]
+    private Color32 numberColor;
 
     //Der Script code des ausgewählten Roboters ohne Änderungen.
     private string oldScriptCode;
@@ -52,15 +59,27 @@ public class RobotProgramPanelManager : MonoBehaviour {
     }
 
     private void Start() {
+        InitializeKeywords();
         StartCoroutine(CheckSyntaxForWholeText());
     }
 
-    IEnumerator CheckSyntaxForWholeText() {
-        TMP_TextInfo textInfo = editor.textComponent.textInfo;
-        int currentCharacter = 0;
+    private void InitializeKeywords() {
+        string[] temp = new string[] {
+            "and", "break", "do", "else", "elseif", "end",
+            "false", "for", "function", "goto", "if", "in",
+            "local", "nil", "not", "or", "repeat", "return",
+            "then", "true", "until", "while"
+        };
+        keywords = new List<string>(temp);
+    }
 
-        Color32[] newVertexColors;
-        Color32 c0 = editor.textComponent.color;
+    /// <summary>
+    /// Geht das ganze Skript des ausgewählten Roboters Wort für Wort durch und überprüft, ob und wie es hervorgehoben werden soll.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CheckSyntaxForWholeText() {
+        //Debug.Log("**Start Syntax Check");
+        TMP_TextInfo textInfo = editor.textComponent.textInfo;
 
         while (true) {
             int characterCount = textInfo.characterCount;
@@ -68,13 +87,49 @@ public class RobotProgramPanelManager : MonoBehaviour {
                 yield return new WaitForSeconds(0.25f);
                 continue;
             }
+
+            int wordCount = textInfo.wordCount;
+            
+            for (int i = 0; i < wordCount; i++) {
+                int charCount = textInfo.wordInfo[i].characterCount;
+                if(charCount == 0) {
+                    continue;
+                }
+                //Debug.Log(textInfo.wordInfo[i].GetWord());
+                CheckSyntax(textInfo, textInfo.wordInfo[i]);
+            }
+            //Debug.Log("**End Syntax Check");
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    /// <summary>
+    /// Überprüft, ob und wie das übergebene Wort des übergebenen Texts hervorgehoben werden soll.
+    /// </summary>
+    /// <param name="textInfo"></param>
+    /// <param name="word"></param>
+    private void CheckSyntax(TMP_TextInfo textInfo, TMP_WordInfo word) {
+        Color32[] newVertexColors;
+        Color32 c0 = editor.textComponent.color;
+
+        for (int i = 0; i < word.characterCount; i++) {
+            int currentCharacter = word.firstCharacterIndex + i;
+            
+            if(Char.IsDigit(textInfo.characterInfo[word.firstCharacterIndex].character) && Char.IsDigit(textInfo.characterInfo[currentCharacter].character)) {
+                c0 = numberColor;
+            } else if(keywords.Contains(word.GetWord())) {
+                c0 = keywordColor;
+            } else {
+                c0 = editor.textComponent.color;
+            }
+
             int materialIndex = textInfo.characterInfo[currentCharacter].materialReferenceIndex;
             newVertexColors = textInfo.meshInfo[materialIndex].colors32;
 
             int vertexIndex = textInfo.characterInfo[currentCharacter].vertexIndex;
 
             if (textInfo.characterInfo[currentCharacter].isVisible) {
-                c0 = highlightedColor;
+                
 
                 newVertexColors[vertexIndex + 0] = c0;
                 newVertexColors[vertexIndex + 1] = c0;
@@ -83,22 +138,7 @@ public class RobotProgramPanelManager : MonoBehaviour {
 
                 editor.textComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
             }
-
-            yield return new WaitForSeconds(0.05f);
         }
-    }
-
-    /// <summary>
-    /// Überprüft, ob das übergebene Wort hervorgehoben werden soll.
-    /// Wenn ja, dann gibt es das Wort zusammen mit den nötigen Rich-Text-Tags zurück, sonst wird nur das Wort zurückgegeben
-    /// </summary>
-    /// <param name="text"></param>
-    /// <returns></returns>
-    private string CheckSyntax(string text) {
-        if(wordsToHighlight.Contains(text)) {
-            text = "<color= #" + ColorUtility.ToHtmlStringRGB(highlightedColor) + ">" + text + "</color>";
-        }
-        return text;
     }
 
     /// <summary>
