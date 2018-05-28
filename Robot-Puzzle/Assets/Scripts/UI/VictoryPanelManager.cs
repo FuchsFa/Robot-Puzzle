a63aceb5e9f05cf9aedcf260f502c0baa100717a
FuchsFa <fabian.tj75@gmail.com>
2018-05-28 14:38:33 +0200

    Score Panel hinzugefügt
    Panel für das Anzeigen des Scores nach erfolgreichem Abschluss eines Levels hinzugefügt.

---
+++ Robot-Puzzle/Assets/Scripts/UI/VictoryPanelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VictoryPanelManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject panelObject;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnVictory() {
+        CalculateScore();
+        ChangeScoreText();
+        ShowScorePanel();
+    }
+
+    private void CalculateScore() {
+
+    }
+
+    private void ChangeScoreText() {
+
+    }
+
+    private void ShowScorePanel() {
+
+    }
+}
