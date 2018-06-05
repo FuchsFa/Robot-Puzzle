46ac61e858437142ce2ce2d3afec1a7b68d6eff6
FuchsFa <fabian.tj75@gmail.com>
2018-06-05 11:13:22 +0200

    Kamerabewegung
    Es sit nun möglich die Kamera mit der Maus oder mit den Pfeiltasten zu bewegen.

---
+++ Robot-Puzzle/Assets/Scripts/Model/Managers/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private float moveSpeed = 1;
+
+    private Vector3 dragOrigin;
+    private Vector3 difference;
+    private bool drag = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(RobotManager.Instance.selectedRobot == null) {
+            HandleMouseDrag();
+        }
+        MoveCamera();
+	}
+
+    /// <summary>
+    /// Kamerabewegung mit Hilfe der Maus
+    /// </summary>
+    private void HandleMouseDrag() {
+        if(Input.GetMouseButton(0)) {
+            difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
+            if(drag == false) {
+                drag = true;
+                dragOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        } else {
+            drag = false;
+        }
+        if(drag == true) {
+            Camera.main.transform.position = dragOrigin - difference;
+        }
+    }
+
+    /// <summary>
+    /// Kamerabewegung mit Hilfe der Tastatur
+    /// </summary>
+    private void MoveCamera() {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+        Camera.main.transform.position += move;
+    }
+}
